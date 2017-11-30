23ec1d6d1d9219d68fe62ede31234be1206eb73c
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-30 16:59:55 -0500

    Added NewRandomRange.

--- common/main.go
+++ common/main.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"math/rand"
+	"time"
+)
+
 // NewRange creates an integer slice of the given length and with sequential
 // values.
 //
@@ -8,6 +13,23 @@ func NewRange(length int) []int {
 	list := make([]int, length)
 	for i := range list {
 		list[i] = i
+	}
+	return list
+}
+
+// NewRandomRange creates an integer slice of the given size and with random
+// values.
+//
+// The time complexity is O(n), where n is the number of elements in the slice.
+func NewRandomRange(length int) []int {
+	seconds := time.Now().UTC().Unix()
+	// This is global state. However, the standard library does provide APIs
+	// for creating local sources and random number generators.
+	rand.Seed(seconds)
+
+	list := make([]int, length)
+	for i := range list {
+		list[i] = rand.Intn(length)
 	}
 	return list
 }
